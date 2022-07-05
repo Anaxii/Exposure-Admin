828342780b54762d86fceb61da839dd403da1aae
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-07-05 15:02:52 -0700

    reorganize commands

--- src/discordbot.js
+++ src/discordbot.js
@@ -28,71 +28,62 @@ function discordBot(discordToken, e, info, DiscordNotifications) {
     return __awaiter(this, void 0, void 0, function* () {
         const { Client, Intents } = require('discord.js');
         const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-        client.once('ready', () => {
-            console.log('Bot loaded');
-        });
-        client.on('messageCreate', (msg) => __awaiter(this, void 0, void 0, function* () {
-            msg.content = msg.content.toLowerCase();
-            const data = msg.content.split(" ");
-            if (data[0] != "!e" || data.length === 1)
-                return;
-            if (data[1] == "epoch") {
+        const commands = {
+            "epoch": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let epoch = e.ExposureObject.methods.epoch().call();
                 msg.channel.send(epoch);
-            }
-            if (data[1] == "epochinfo") {
+            }),
+            "epochinfo": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let epochInfo = yield info.epochNotification(false);
                 msg.channel.send(epochInfo);
-            }
-            if (data[1] == "nextepoch") {
+            }),
+            "nextepoch": () => __awaiter(this, void 0, void 0, function* () {
                 yield info.epochNotification(true);
                 yield e.nextEpoch();
                 yield info.epochNotification(true);
-            }
-            if (data[1] == "newetf") {
-                msg.channel.send("Starting new ETF");
-                let address = yield e.newETF("TEST", "TEST");
-                msg.channel.send("New ETF initialized at " + address);
-            }
-            if (data[1] == "prices" || data[1] == "mcaps") {
+            }),
+            "prices": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let prices = yield info.getPricesAndMcaps();
                 let p = "Prices: \n";
-                let m = "MCAPs: \n";
                 for (const i in prices.prices) {
                     p += i + ": " + prices.prices[i].toLocaleString() + "\n";
-                    m += i + ": " + prices.mcaps[i].toLocaleString() + "\n";
                 }
-                if (data[1] == "prices") {
-                    let ep = "Exposure Prices: \n";
-                    for (const i in e.Tokens) {
-                        let pr = yield info.getExposurePrice(e.Tokens[i].tokenAddress);
-                        ep += e.Tokens[i].token + ": " + pr.toLocaleString() + "\n";
-                    }
-                    msg.channel.send(p);
-                    msg.channel.send(ep);
+                let ep = "Exposure Prices: \n";
+                for (const i in e.Tokens) {
+                    let pr = yield info.getExposurePrice(e.Tokens[i].tokenAddress);
+                    ep += e.Tokens[i].token + ": " + pr.toLocaleString() + "\n";
+                }
+                msg.channel.send(p);
+                msg.channel.send(ep);
+            }),
+            "mcaps": (msg, data) => __awaiter(this, void 0, void 0, function* () {
+                let prices = yield info.getPricesAndMcaps();
+                let m = "MCAPs: \n";
+                for (const i in prices.prices) {
+                    m += i + ": " + prices.mcaps[i].toLocaleString() + "\n";
                 }
                 if (data[1] == "mcaps")
                     msg.channel.send(m);
-            }
-            if (data[1] == "balances") {
+            }),
+            "balances": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let balances = yield info.getAllExposureTokenBalances(e.ExposureAddress);
                 let m = "Balances: \n";
                 for (const i in balances) {
                     m += i + ": " + balances[i].toLocaleString() + "\n";
                 }
                 msg.channel.send(m.toLocaleString());
-            }
-            if (data[1] == "shares") {
+            }),
+            "shares": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let shareBalance = yield e.ExposureObject.methods.totalSupply().call();
                 msg.channel.send((BigInt(shareBalance) / BigInt(10 ** 18)).toLocaleString());
-            }
-            if (data[1] == "nav") {
+            }),
+            "nav": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let nav = yield info.getNAV();
                 let shareBalance = yield e.ExposureObject.methods.totalSupply().call();
                 msg.channel.send("$" + nav.toLocaleString());
                 msg.channel.send("NAV Per Share: $" + (nav / (Number(BigInt(shareBalance) / BigInt(10 ** 16)) / 100)).toLocaleString());
-            }
-            if (data[1] == "index") {
+            }),
+            "index": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let values = yield Promise.all([
                     info.getIndexPrice(),
                     info.actualIndexPrice(),
@@ -100,16 +91,16 @@ function discordBot(discordToken, e, info, DiscordNotifications) {
                     info.tokenPortionIndex(true)
                 ]);
                 msg.channel.send("Index Price:                                $" + values[0].toLocaleString() + "\nPortion Index Price:                  $" + values[1].toLocaleString() + "\nTracked Portion Index Price:   $" + values[2].toLocaleString() + "\nTrue Index Price:                       $" + values[3].toLocaleString());
-            }
-            if (data[1] == "portions") {
+            }),
+            "portions": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let m = "";
                 for (const i in e.Tokens) {
                     let divisor = yield e.ExposureObject.methods.getTokenPortions(e.CurrentEpoch, e.Tokens[i].tokenAddress).call();
                     m += e.Tokens[i].token + ": " + (BigInt(divisor) / BigInt(10 ** 18)).toLocaleString() + "\n";
                 }
                 msg.channel.send(m);
-            }
-            if (data[1] == "notif") {
+            }),
+            "notif": (msg, data) => __awaiter(this, void 0, void 0, function* () {
                 if (data[2] == "on") {
                     DiscordNotifications = true;
                     msg.channel.send("Webhook notifications turned on");
@@ -119,42 +110,51 @@ function discordBot(discordToken, e, info, DiscordNotifications) {
                     msg.channel.send("Webhook notifications turned off");
                 }
                 yield util_1.editConfig("discordNotifications", DiscordNotifications);
-            }
-            if (data[1] == "newetf") {
-                yield e.newETF(data[1], data[2]);
-            }
-            if (data[1] == "editconfig" && data.length > 3) {
+            }),
+            "newetf": (msg, data) => __awaiter(this, void 0, void 0, function* () {
+                msg.channel.send("Starting new ETF");
+                let address = yield e.newETF(data[1], data[2]);
+                msg.channel.send("New ETF initialized at " + address);
+            }),
+            "editconfig": (msg, data) => __awaiter(this, void 0, void 0, function* () {
                 yield util_1.editConfig(data[2], data[3]);
                 msg.channel.send("Updated config for " + data[2]);
-            }
-            if (data[1] == "reboot") {
+            }),
+            "reboot": (msg) => __awaiter(this, void 0, void 0, function* () {
                 yield msg.channel.send("Rebooting bot");
                 yield util_1.editConfig("reboot", true);
                 process.exit(1);
-            }
-            if (data[1] == "exposure") {
-                yield msg.channel.send(e.ExposureAddress);
-            }
-            if (data[1] == "mint") {
+            }),
+            "mint": (msg, data) => __awaiter(this, void 0, void 0, function* () {
                 let shareBalance = yield e.ExposureObject.methods.balanceOf(e.PublicKey).call();
                 yield msg.channel.send("Initial balance: " + (Number(BigInt(shareBalance) / BigInt(10 ** 14)) / (10 ** 4)).toLocaleString());
                 let newBal = yield e.mintShares(BigInt(Number(data[2]) * 10 ** 17));
                 yield msg.channel.send("New balance: " + (Number(BigInt(newBal) / BigInt(10 ** 14)) / (10 ** 4)).toLocaleString());
-            }
-            if (data[1] == "burn") {
+            }),
+            "burn": (msg, data) => __awaiter(this, void 0, void 0, function* () {
                 let shareBalance = yield e.ExposureObject.methods.balanceOf(e.PublicKey).call();
                 yield msg.channel.send("Initial balance: " + (Number(BigInt(shareBalance) / BigInt(10 ** 14)) / (10 ** 4)).toLocaleString());
                 let newBal = yield e.burnShares(BigInt(Number(data[2]) * 10 ** 17));
                 yield msg.channel.send("New balance: " + (Number(BigInt(newBal) / BigInt(10 ** 14)) / (10 ** 4)).toLocaleString());
-            }
-            if (data[1] == "sharebalance") {
+            }),
+            "sharebalance": (msg) => __awaiter(this, void 0, void 0, function* () {
                 let shareBalance = yield e.ExposureObject.methods.balanceOf(e.PublicKey).call();
                 yield msg.channel.send("Balance: " + (Number(BigInt(shareBalance) / BigInt(10 ** 14)) / (10 ** 4)).toLocaleString());
-            }
-            if (data[1] == "help") {
+            }),
+            "help": (msg) => __awaiter(this, void 0, void 0, function* () {
                 yield msg.channel.send("epoch \nepochinfo \nnextepoch \nnewetf \nprices \nmcaps \nbalances \nshares \nnav \nindex \nportions \nnotif (on/off) \nnewetf (name) (symbol) \neditconfig (data) \nrebbot \nexposure \nmint (amount) \nburn (amount) \nsharebalance ");
-            }
-        }));
+            })
+        };
+        client.once('ready', () => {
+            console.log('Bot loaded');
+        });
+        client.on('messageCreate', (msg) => {
+            msg.content = msg.content.toLowerCase();
+            const data = msg.content.split(" ");
+            if (data[0] != "!e" || data.length === 1)
+                return;
+            commands[data[1]](msg, data);
+        });
         client.login(discordToken);
     });
 }
